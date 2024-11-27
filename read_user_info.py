1f45b68a92db8d31f23aa6a9d55f484a950e71dc
Owenjohn2223 <owenjohngalamgam@gmail.com>
2024-11-27 22:58:08 +0800

    added some '-' for readability

--- read_user_info.py
+++ read_user_info.py
@@ -11,15 +11,20 @@ for line in file_lines:
     line = line.strip() 
     if line == "":
         if any(search_info in item.upper() for item in store_info):
+            readabilty = (f"-----Search result for '{search_info}'-----")
+            print(readabilty)
             found = True
-            print("\n".join(store_info))  
+            print("\n".join(store_info))
+            print("-" * len(readabilty))
         store_info = []
     else:
         store_info.append(line)  
         
 if store_info and any(search_info in item.upper() for item in store_info):
+    print(readabilty)
     found = True
     print("\n".join(store_info))
+    print("-" * len(readabilty))
 
 if not found:
     print(f"No result found for '{search_info}'")        
