82190c3c08b0a8ce2cc2d1ae7ee971fe92b3cdfc
Owenjohn2223 <owenjohngalamgam@gmail.com>
2024-11-27 22:10:20 +0800

    properly reads info i want to be displayed, but displays excess info from another line in txt

--- read_user_info.py
+++ read_user_info.py
@@ -1,2 +1,23 @@
+
 with open("file_informations.txt", "r") as file_infos:
-    print(file_infos.read())
+    file_lines = file_infos.readlines()
+
+search_info = input("Input a name to search: ").strip().upper()
+
+found = False
+store_info = []
+
+for line in file_lines:
+    if line.strip() == "":
+        if any(search_info in item.upper() for item in store_info):
+            found = True
+            print("\n".join(store_info))
+            store_info = []
+    else:
+        store_info.append(line.strip())
+
+if not found:
+     print(f"No result found for '{search_info}'")
+        
+
+
